c54078f28e4e9df442e427bd35c9d35e3a97907f
Shabana <107318445+Art3mis-s@users.noreply.github.com>
2023-06-10 09:37:00 +0200

    Delete hours.py

--- hours.py
+++
@@ -1,7 +0,0 @@
-#prompt the user to input Hours and rate
-hours = input("Enter Hours: ")
-rate = input("Enter Rate: ")
-#computing gross pay<--
-pay = float(hours) * float(rate)
-#payment
-print("Pay:", pay)
