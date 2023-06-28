beb8ea12a3d9fb54ebfcbcc2d3c03329a1a4dc2e
Shabana <107318445+Art3mis-s@users.noreply.github.com>
2023-06-28 23:55:25 +0200

    Date Formats
    This Python code converts a user-entered date to the "YYYY-MM-DD" format. It handles both "month-day-year" and "Month day, year" formats, validating the input and removing any spaces between the month and day. The converted date is then printed.

---
+++ outdated.py
@@ -0,0 +1,43 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+while True:
+    date = input("Date: ")
+    try:
+        month, day, year = date.split("/")
+        if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
+            break
+    except:
+        try:
+            old_month, old_day, year = date.split(" ")
+            for i in range(len(months)):
+                if old_month == months[i]:
+                    month = i + 1
+
+            if not old_day.endswith(","):
+                continue
+            day = old_day.replace(",","")
+            if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
+                break
+        except:
+            print()
+            pass
+
+print(f"{year}-{int(month):02}-{int(day):02}")
+
+
+
+
+
